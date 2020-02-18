af42a3597051876b469ecaef8550fed9755f0edf
TashfeenRao <raotashfeen25@gmail.com>
2020-02-19 02:32:01 +0500

    added remeber_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_18_201309) do
+ActiveRecord::Schema.define(version: 2020_02_18_212904) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_18_201309) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "remember_digest"
   end
 
 end
