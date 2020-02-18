6ec28e86a3ba0ee93b48c6dd8dc2370a286a77c7
TashfeenRao <raotashfeen25@gmail.com>
2020-02-19 02:41:56 +0500

    new token and remember method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {maximum: 30}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +9,20 @@ class User < ApplicationRecord
                uniqueness: {case_sensitive: false}
     validates :password, length: {minimum: 6}, presence: true
     has_secure_password
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+        Secure.Randome.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attributes(:remember_digest,User.digest(remember_token))  
+    end
+
 end
