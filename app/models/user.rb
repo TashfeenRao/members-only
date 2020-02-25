96490ab1c1f1e9153d57dae97c6ca3f6c7e93c0e
TashfeenRao <raotashfeen25@gmail.com>
2020-02-26 03:40:27 +0500

    cookies sign in added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
-  before_create :remember_token
+  before_create :create_remember_digest
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
@@ -12,8 +12,7 @@ class User < ApplicationRecord
   has_many :posts
 
   def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    Digest::SHA1.hexdigest string
   end
 
   def self.new_token
@@ -31,5 +30,12 @@ class User < ApplicationRecord
 
   def authenticated(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def create_remember_digest
+    self.remember_token = User.new_token
+    self.remember_digest = User.digest(remember_token)
   end
 end
