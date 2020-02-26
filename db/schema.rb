711ce4868d4ed53a1af2fd0bdeccca2698063897
TashfeenRao <raotashfeen25@gmail.com>
2020-02-26 20:04:01 +0500

    all post button added

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_200657) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
